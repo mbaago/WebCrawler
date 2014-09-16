2a987a8fc3cb542174a66b55d1f4a3522c9ffad7
mbaago <hypnox@hypnox.dk>
2014-09-16 10:34:32 +0200

    database and stuff

--- Peter/Program.cs
+++ Peter/Program.cs
@@ -8,17 +8,41 @@ namespace Peter
 {
     class Program
     {
+        static DataClasses1DataContext db = new DataClasses1DataContext();
         static void Main(string[] args)
         {
-            Crawler.Crawler craw = new Crawler.Crawler(10);
+                
+            //Crawler.Crawler craw = new Crawler.Crawler(10);
 
-            string url = "httP://rDddit.com/r/stupid/%a0%05%7e/";
+            //string url = "httP://rDddit.com/r/stupid/%a0%05%7e/";
 
-            string res = craw.MakeURLPretty(url);
+            //string res = craw.MakeURLPretty(url);
 
-            Console.WriteLine(res);
+            //Console.WriteLine(res);
+
+            test();
 
             Console.ReadKey();
+        }
+
+        static void test()
+        {
+            Page[] pagesA = { new Page { url = "www", html = "www" }, new Page { url = "www.reddit.com", html = "www" } };
+            Page page = new Page { url = "www", html = "www" };
+            //Page page2 = new Page { url = "www.reddit.com", html = "www" };
+            //db.Pages.InsertAllOnSubmit(pagesA);
+            db.Pages.InsertOnSubmit(page);
+
+            db.SubmitChanges();
+
+            IEnumerable<Page> pages = (from p in db.Pages
+                                       select p);
+
+            foreach (Page p in pages)
+            {
+                Console.WriteLine(p.Id + " " + p.url);
+            }
+            
         }
     }
 }
