6c6658087dbf2de2673cfefcfbdc590fdb5d32fd
estj92 <estj92@gmail.com>
2014-09-30 11:07:49 +0200

    crawling crawling crawling

--- Crawler/Crawler.cs
+++ Crawler/Crawler.cs
@@ -13,135 +13,48 @@ namespace Crawler
 {
     public class Crawler
     {
-        public Crawler(int numFrontQueues, int numBackQueues, TimeSpan timebetweenVisits, TimeSpan maxAgeOfRobots, IEnumerable<PrettyURL> seed)
+        public Crawler(int numFrontQueues, int numBackQueues, TimeSpan timebetweenVisits, TimeSpan maxAgeOfRobots, IEnumerable<PrettyURL> seed, Queue<Tuple<PrettyURL, string, DateTime>> queue)
         {
-            IAMAROBOTHANDLER = new RobotsStuff(maxAgeOfRobots);
-            IAMTHEMERCATOR = new Mercator(numFrontQueues, numBackQueues, timebetweenVisits, seed);
+            Robot_IAm = new RobotsStuff(maxAgeOfRobots);
+            Mercator_IAm = new Mercator(numFrontQueues, numBackQueues, timebetweenVisits, seed);
+            CrawledQueue = queue;
+
+            ContinueCrawling = true;
         }
 
-        private RobotsStuff IAMAROBOTHANDLER { get; set; }
-        private Mercator IAMTHEMERCATOR { get; set; }
-        private MainIndexer IAMTHEINDEXER { get; set; }
-        private Jaccard IAMJACCARD = new Jaccard(4, 0.9);
+        public void Crawl()
+        {
+            while (ContinueCrawling)
+            {
+                var site = Mercator_IAm.GetURLToCrawl();
+                var html = DownloadHTML(site);
+                var toQueue = Tuple.Create(site, html, DateTime.Now);
+                CrawledQueue.Enqueue(toQueue);
 
+                Debug.WriteLine("Crawler: downloaded " + site.GetPrettyURL);
+
+                var links = ExtractLinksFromHTML(site, html);
+                foreach (var link in links)
+                {
+                    Mercator_IAm.AddURLToFrontQueue(link);
+                }
+            }
+        }
+
+        private Queue<Tuple<PrettyURL, string, DateTime>> CrawledQueue { get; set; }
+
+        private RobotsStuff Robot_IAm { get; set; }
+        private Mercator Mercator_IAm { get; set; }
+        //private MainIndexer IAMTHEINDEXER { get; set; }
+        //private Jaccard IAMJACCARD = new Jaccard(4, 0.9);
         private DB DataBase = new DB();
-
-        public Dictionary<string, string> CrawlTheWebAndAddToDB(int getSites)
-        {
-            //System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
-            //watch.Start();
-
-            Console.WriteLine("Downloading");
-            return DoTheCrawl_SaveToDB(getSites, false);
-
-            //watch.Stop();
-        }
-
-        private Dictionary<string, string> DoTheCrawl_SaveToDB(int numberOfSitesToVisit, bool print)
-        {
-            Stopwatch watch = new Stopwatch();
-            Stopwatch totalTime = new Stopwatch();
-            Stopwatch dupTime = new Stopwatch();
-
-            Dictionary<string, TimeSpan> times = new Dictionary<string, TimeSpan>();
-            List<PrettyURL> hasNearDuplicate = new List<PrettyURL>();
-
-            Dictionary<string, string> result = new Dictionary<string, string>();
-            int sitesVisited = 0;
-
-            totalTime.Start();
-
-            while (sitesVisited < numberOfSitesToVisit)
-            {
-                watch.Restart();
-
-                var url = IAMTHEMERCATOR.GetURLToCrawl();
-
-                // check with robot
-                if (IAMAROBOTHANDLER.IsVisitAllowed(url))
-                {
-                    var html = DownloadHTML(url);
-                    if (html == null)
-                    {
-                        continue;
-                    }
-
-                    // check for near dups
-                    dupTime.Restart();
-                    if (IsNearDuplicateOfAlreadyAddedSite(html))
-                    {
-                        hasNearDuplicate.Add(url);
-                        continue;
-                    }
-                    dupTime.Stop();
-
-                    DataBase.InsertNewDownloadedPage(url.GetPrettyURL, html);
-
-                    var links = ExtractLinksFromHTML(url, html);
-                    foreach (var link in links)
-                    {
-                        IAMTHEMERCATOR.AddURLToFrontQueue(link);
-                    }
-
-                    if (print)
-                    {
-                        Console.WriteLine(sitesVisited + "\t" + url);
-                    }
-                }
-
-                watch.Stop();
-                times[url.GetDomain] = watch.Elapsed;
-                Console.WriteLine(sitesVisited + "\t" + (int)watch.Elapsed.TotalMilliseconds + "\t" + (int)dupTime.Elapsed.TotalMilliseconds + "\t" + url);
-                Console.Title = totalTime.Elapsed.ToString();
-                sitesVisited++;
-            }
-
-            return result;
-        }
-
-        private bool IsNearDuplicateOfAlreadyAddedSite(string html)
-        {
-            // smid html i db
-            var input = GetHTMLContent(html);
-
-            // smid shingle hashes i db?
-            foreach (var site in DataBase.GetAllPages())
-            {
-                var compareWith = GetHTMLContent(site.html);
-
-                if (IAMJACCARD.IsNearDuplicate(compareWith, input))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private string GetHTMLContent(string html)
-        {
-            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html);
-
-            var a = doc.DocumentNode.SelectNodes("//p");
-            if (a == null)
-            {
-                return "";
-            }
-            var b = a.Select(p => p.InnerText);
-
-
-            var content = string.Join(" ", b);
-
-            return content;
-        }
-
+        public bool ContinueCrawling { get; set; }
 
         public string DownloadHTML(PrettyURL url)
         {
             WebClient web = new WebClient();
 
-            if (IAMAROBOTHANDLER.IsVisitAllowed(url))
+            if (Robot_IAm.IsVisitAllowed(url))
             {
                 try
                 {
@@ -162,6 +75,11 @@ namespace Crawler
 
         public IEnumerable<PrettyURL> ExtractLinksFromHTML(PrettyURL url, string html)
         {
+            if (html == null)
+            {
+                return Enumerable.Empty<PrettyURL>();
+            }
+
             var hrefs = html.Split(new string[] { "<a href=\"" }, StringSplitOptions.RemoveEmptyEntries)
                 .Skip(1)
                 .Where(s => !s.StartsWith("feed") && !s.StartsWith("javascript"));
@@ -185,5 +103,106 @@ namespace Crawler
 
             return urls;
         }
+
+        //private Dictionary<string, string> DoTheCrawl_SaveToDB(int numberOfSitesToVisit, bool print)
+        //{
+        //    Stopwatch watch = new Stopwatch();
+        //    Stopwatch totalTime = new Stopwatch();
+        //    Stopwatch dupTime = new Stopwatch();
+
+        //    Dictionary<string, TimeSpan> times = new Dictionary<string, TimeSpan>();
+        //    List<PrettyURL> hasNearDuplicate = new List<PrettyURL>();
+
+        //    Dictionary<string, string> result = new Dictionary<string, string>();
+        //    int sitesVisited = 0;
+
+        //    totalTime.Start();
+
+        //    while (sitesVisited < numberOfSitesToVisit)
+        //    {
+        //        watch.Restart();
+
+        //        var url = Mercator_IAm.GetURLToCrawl();
+
+        //        // check with robot
+        //        if (Robot_IAm.IsVisitAllowed(url))
+        //        {
+        //            var html = DownloadHTML(url);
+        //            if (html == null)
+        //            {
+        //                continue;
+        //            }
+
+        //            // check for near dups
+        //            dupTime.Restart();
+        //            if (IsNearDuplicateOfAlreadyAddedSite(html))
+        //            {
+        //                hasNearDuplicate.Add(url);
+        //                continue;
+        //            }
+        //            dupTime.Stop();
+
+        //            DataBase.InsertNewDownloadedPage(url.GetPrettyURL, html);
+
+        //            var links = ExtractLinksFromHTML(url, html);
+        //            foreach (var link in links)
+        //            {
+        //                Mercator_IAm.AddURLToFrontQueue(link);
+        //            }
+
+        //            if (print)
+        //            {
+        //                Console.WriteLine(sitesVisited + "\t" + url);
+        //            }
+        //        }
+
+        //        watch.Stop();
+        //        times[url.GetDomain] = watch.Elapsed;
+        //        Console.WriteLine(sitesVisited + "\t" + (int)watch.Elapsed.TotalMilliseconds + "\t" + (int)dupTime.Elapsed.TotalMilliseconds + "\t" + url);
+        //        Console.Title = totalTime.Elapsed.ToString();
+        //        sitesVisited++;
+        //    }
+
+        //    return result;
+        //}
+
+        //private bool IsNearDuplicateOfAlreadyAddedSite(string html)
+        //{
+        //    // smid html i db
+        //    var input = GetHTMLContent(html);
+
+        //    // smid shingle hashes i db?
+        //    foreach (var site in DataBase.GetAllPages())
+        //    {
+        //        var compareWith = GetHTMLContent(site.html);
+
+        //        if (IAMJACCARD.IsNearDuplicate(compareWith, input))
+        //        {
+        //            return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
+        //private string GetHTMLContent(string html)
+        //{
+        //    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+        //    doc.LoadHtml(html);
+
+        //    var a = doc.DocumentNode.SelectNodes("//p");
+        //    if (a == null)
+        //    {
+        //        return "";
+        //    }
+        //    var b = a.Select(p => p.InnerText);
+
+
+        //    var content = string.Join(" ", b);
+
+        //    return content;
+        //}
+
+
     }
 }
--- Peter/Program.cs
+++ Peter/Program.cs
@@ -12,53 +12,68 @@ namespace Peter
 {
     class Program
     {
-        static DBContextDataContext dbCon = new DBContextDataContext();
+        //static DBContextDataContext dbCon = new DBContextDataContext();
+        static DB database = new DB();
+        private static Queue<Tuple<PrettyURL, string, DateTime>> ToBeIndexedQueue = new Queue<Tuple<PrettyURL, string, DateTime>>();
+
+
         static void Main(string[] args)
         {
-            //database.clearPages();
-            //return;
+            int sitesToCrawl = 5;
+            int numFrontQueues = 10;
+            int numBackQueues = 3;
+            TimeSpan timeBetweenHits = TimeSpan.FromSeconds(1);
+            TimeSpan maxRobotAge = TimeSpan.FromMinutes(5);
+            var seed = new PrettyURL[] { new PrettyURL("newz.dk"), new PrettyURL("aau.dk"), new PrettyURL("politikken.dk") };
+            Crawler.Crawler crawler = new Crawler.Crawler(numFrontQueues, numBackQueues, timeBetweenHits, maxRobotAge, seed, ToBeIndexedQueue);
 
-            if (database.GetAllPages().Count() == 0)
+            System.Threading.Thread thread = new System.Threading.Thread(crawler.Crawl);
+            thread.Start();
+
+            while (ToBeIndexedQueue.Count < sitesToCrawl)
             {
-
-                Console.WriteLine("Started");
-                int sitesToCrawl = 1000;
-                int numFrontQueues = 10;
-                int numBackQueues = 3;
-                TimeSpan timeBetweenHits = TimeSpan.FromSeconds(1);
-                TimeSpan maxRobotAge = TimeSpan.FromMinutes(5);
-                var seed = new PrettyURL[] { new PrettyURL("newz.dk"), new PrettyURL("aau.dk"), new PrettyURL("politikken.dk") };
-
-                Crawler.Crawler crawler = new Crawler.Crawler(numFrontQueues, numBackQueues, timeBetweenHits, maxRobotAge, seed);
-                var sites = crawler.CrawlTheWebAndAddToDB(sitesToCrawl);
-                Console.WriteLine("Completed downloading");
+                System.Threading.Thread.Sleep(100);
             }
 
-            Console.WriteLine("Creating index");
-            var stopWords = new string[] { "og", "i", "jeg", "det", "at", "en", "den", "til", "er", "som", "på", "de", "med", "han", "af", "for", "ikke", "der", "var", "mig", "sig", "men", "et", "har", "om", "vi", "min", "havde", "ham", "hun", "nu", "over", "da", "fra", "du", "ud", "sin", "dem", "os", "op", "man", "hans", "hvor", "eller", "hvad", "skal", "selv", "her", "alle", "vil", "blev", "kunne", "ind", "når", "være", "dog", "noget", "ville", "jo", "deres", "efter", "ned", "skulle", "denne", "end", "dette", "mit", "også", "under", "have", "dig", "anden", "hende", "mine", "alt", "meget", "sit", "sine", "vor", "mod", "disse", "hvis", "din", "nogle", "hos", "blive", "mange", "ad", "bliver", "hendes", "været", "thi", "jer", "sådan" };
-            var charsToRemove = new char[] { ',', '.', '?' };
-            MainIndexer indexer = new MainIndexer(stopWords, charsToRemove);
-            var index = indexer.CreateInverseIndex();
-            Console.WriteLine("Indexing completed");
+            crawler.ContinueCrawling = false;
 
-            Console.WriteLine(index.Select(t => t.Value.Count).Sum());
+            Console.WriteLine("yo");
 
-            Console.ReadKey();
+            Console.ReadLine();
         }
 
-        static DB database = new DB();
+        //static void Main(string[] args)
+        //{
+        //    //database.clearPages();
+        //    //return;
 
-        static void test()
-        {
-            DB db = new DB();
+        //    if (database.GetAllPages().Count() == 0)
+        //    {
 
-            db.clearPages();
-            db.insertNew("www.reddit.com", "<p>bla bla bla</p>");
+        //        Console.WriteLine("Started");
+        //        int sitesToCrawl = 25;
+        //        int numFrontQueues = 10;
+        //        int numBackQueues = 3;
+        //        TimeSpan timeBetweenHits = TimeSpan.FromSeconds(1);
+        //        TimeSpan maxRobotAge = TimeSpan.FromMinutes(5);
+        //        var seed = new PrettyURL[] { new PrettyURL("newz.dk"), new PrettyURL("aau.dk"), new PrettyURL("politikken.dk") };
 
+        //        Crawler.Crawler crawler = new Crawler.Crawler(numFrontQueues, numBackQueues, timeBetweenHits, maxRobotAge, seed);
+        //        var sites = crawler.CrawlTheWebAndAddToDB(sitesToCrawl);
+        //        Console.WriteLine("Completed downloading");
+        //    }
 
-            Page page = db.getPageOnUrl("www.reddit.com");
+        //    Console.WriteLine("Creating index");
+        //    var stopWords = new string[] { "og", "i", "jeg", "det", "at", "en", "den", "til", "er", "som", "på", "de", "med", "han", "af", "for", "ikke", "der", "var", "mig", "sig", "men", "et", "har", "om", "vi", "min", "havde", "ham", "hun", "nu", "over", "da", "fra", "du", "ud", "sin", "dem", "os", "op", "man", "hans", "hvor", "eller", "hvad", "skal", "selv", "her", "alle", "vil", "blev", "kunne", "ind", "når", "være", "dog", "noget", "ville", "jo", "deres", "efter", "ned", "skulle", "denne", "end", "dette", "mit", "også", "under", "have", "dig", "anden", "hende", "mine", "alt", "meget", "sit", "sine", "vor", "mod", "disse", "hvis", "din", "nogle", "hos", "blive", "mange", "ad", "bliver", "hendes", "været", "thi", "jer", "sådan" };
+        //    var charsToRemove = new char[] { ',', '.', '?' };
+        //    MainIndexer indexer = new MainIndexer(stopWords, charsToRemove);
+        //    var index = indexer.CreateInverseIndex();
+        //    Console.WriteLine("Indexing completed");
 
-            Console.WriteLine(page.id + " " + page.url + " " + page.html);
-        }
+        //    Console.WriteLine(index.Select(t => t.Value.Count).Sum());
+
+        //    Console.ReadKey();
+        //}
+
     }
 }
