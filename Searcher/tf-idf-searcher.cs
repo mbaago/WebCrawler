afe3e24cdc417ff637b85b33cbfa94166c2a1d44
estj92 <estj92@gmail.com>
2014-10-01 17:29:59 +0200

    Forgot to add the project.

---
+++ Searcher/tf-idf-searcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetersWeb;
+
+namespace Peter
+{
+    public class tf_idf_searcher
+    {
+        private DB Database { get; set; }
+
+        public tf_idf_searcher()
+        {
+            Database = new DB();
+        }
+
+        public double TF_IDF(string term, string prettyURL)
+        {
+            int tf1 = 1 + Database.TermFrequencyInDocument(term, prettyURL);
+            double tf = Math.Log10(tf1);
+
+            int N = Database.TotalSitesInDB();
+            int df1 = Database.DocumentFrequency(term);
+            double df2 = N / df1;
+            double df = Math.Log10(df2);
+
+            double result = tf * df;
+
+            return result;
+        }
+    }
+}
